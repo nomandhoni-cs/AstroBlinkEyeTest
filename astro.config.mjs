07eb3304510ac8da96fed8ceaa311b78b4c03da6
Noman Dhoni <92979541+nomandhoni-cs@users.noreply.github.com>
2024-11-24 23:25:37 +0000

    Testing api

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import dotenv from "dotenv";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 dotenv.config(); // Load .env variables
 export default defineConfig({
-  output: "server",
+  output: "hybrid",
   adapter: vercel(),
   integrations: [
     react(),
